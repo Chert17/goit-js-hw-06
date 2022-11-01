8c95f4112222c90df66fae707ce25d134ee25a75
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-01 23:44:41 +0200

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsItems = ingredients.map((item) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = item;
+
+  return itemEl;
+});
+
+ingredientsList.append(...ingredientsItems);
+
+console.log(ingredientsList);
