65051d32f15f5940ac05008a59b28ecd067cb948
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-03 10:54:27 +0200

    Task-4

---
+++ js/task-04.js
@@ -0,0 +1,35 @@
+const decr = document.querySelector("[data-action=decrement]");
+const incr = document.querySelector("[data-action=increment]");
+const valEl = document.querySelector("#value");
+
+let value = 0;
+
+incr.addEventListener("click", function () {
+  value += 1;
+  valEl.textContent = value;
+});
+
+decr.addEventListener("click", function () {
+  value -= 1;
+  valEl.textContent = value;
+});
+
+// const counter = {
+//   value: 0,
+//   increment() {
+//     this.value += 1;
+//   },
+//   decrement() {
+//     this.value -= 1;
+//   },
+// };
+
+// decr.addEventListener("click", function () {
+//   counter.decrement();
+//   valEl.textContent = counter.value;
+// });
+
+// incr.addEventListener("click", function () {
+//   counter.increment();
+//   valEl.textContent = counter.value;
+// });
