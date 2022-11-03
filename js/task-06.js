a00eae2eb47c8e43d676cab761092e8bc09767ed
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-03 16:18:22 +0200

    Task-6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+const lengthInput = Number(input.dataset.length);
+input.addEventListener("blur", onVerificationInput);
+
+function onVerificationInput(event) {
+  event.currentTarget.value.length <= lengthInput
+    ? input.classList.add("valid")
+    : input.classList.add("invalid");
+}
+
+//   if (event.currentTarget.value.length <= lengthInput) {
+//     return input.classList.add("valid");
+//   }
+//   return input.classList.add("invalid");
