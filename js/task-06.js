04a2fdfc29b8ceb74983dc2e4e75f8188feba244
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-07 17:48:04 +0200

    fix/task-6

--- js/task-06.js
+++ js/task-06.js
@@ -4,12 +4,10 @@ const lengthInput = Number(input.dataset.length);
 input.addEventListener("blur", onVerificationInput);
 
 function onVerificationInput(event) {
-  event.currentTarget.value.length <= lengthInput
-    ? input.classList.add("valid")
-    : input.classList.add("invalid");
+  if (event.currentTarget.value.length === lengthInput) {
+    event.target.classList.remove("invalid");
+    event.target.classList.add("valid");
+    return;
+  }
+  event.target.classList.add("invalid");
 }
-
-//   if (event.currentTarget.value.length <= lengthInput) {
-//     return input.classList.add("valid");
-//   }
-//   return input.classList.add("invalid");
